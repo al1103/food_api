998e375253c44b309c3318f7a448fb1e41199cd5
Son <sonit251203@gmail.com>
2025-03-30 01:23:40 +0700

    Refactor environment configuration and database connection

--- config/database.js
+++ config/database.js
@@ -21,6 +21,7 @@ pool
   .then((client) => {
     console.log("🚀 Connected to PostgreSQL database");
     client.release(); // Release the client back to the pool
+    
   })
   .catch((err) => {
     console.error("❌ Database Connection Failed:", err);
